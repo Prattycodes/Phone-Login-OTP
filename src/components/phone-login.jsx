230babf81f79cde734613d87f4224bc1b01c06c1
Prattycodes <prathamtetgure883@gmail.com>
2024-01-21 16:46:16 +0530

    Code Added

---
+++ src/components/phone-login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import OtpInput from "./otp-input";
+
+const PhoneOtpForm = () => {
+
+    const[phoneNumber, setPhoneNumber]= useState("");
+    const[showOtpInput, setShowOtpInput]= useState(false);
+    const handlePhoneNumber= (event)=>{
+        setPhoneNumber(event.target.value);
+    };
+    const handlePhoneSubmit= (event)=>{
+        event.preventDefault();
+
+        //Phone Validation 
+        const regex= /[^0-9]/g;
+        if(phoneNumber.length<10 || regex.test(phoneNumber)){
+            alert("Invalid Phone Number");
+            return;
+        }
+
+        //Call BE API
+        //Show OTP Field
+        setShowOtpInput(true);
+    };
+
+    const onOtpSubmit= (otp)=>{
+        console.log("Login Successfull", otp)
+    };
+  return (
+    <div>
+        {!showOtpInput? (<form onSubmit={handlePhoneSubmit}>
+            <input type="text" value= {phoneNumber} onChange={handlePhoneNumber} placeholder= "Enter Phone Number"/>
+            <button type="submit">Submit</button>
+        </form>): 
+        (<div><p>Enter OTP sent to {phoneNumber}</p>
+        <OtpInput length={4} onOtpSubmit= {onOtpSubmit}/>
+        </div>
+        )}
+    </div>
+  )
+}
+
+export default PhoneOtpForm
